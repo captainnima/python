26d42d8439ee63f67f4dbff42bb7499221b75e32
Nima Zahadat, Ph.D <30416653+captainnima@users.noreply.github.com>
2022-10-15 18:57:23 -0400

    Add files via upload

---
+++ 009 Modules.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import this
+
+
+# In[2]:
+
+
+print(abs(-3))
+
+
+# In[6]:
+
+
+import sys
+
+
+# In[7]:
+
+
+sys.path
+
+
+# In[8]:
+
+
+import my_module
+
+
+# In[9]:
+
+
+import my_module
+
+
+# In[10]:
+
+
+coffee_func('Porky')
+
+
+# In[11]:
+
+
+my_module.coffee_func('Porky')
+
+
+# In[12]:
+
+
+my_module.message
+
+
+# In[13]:
+
+
+import my_module as mm
+
+
+# In[14]:
+
+
+mm.coffee_func('Daffy')
+
+
+# In[15]:
+
+
+mm.coffee_func()
+
+
+# In[16]:
+
+
+from my_module import coffee_func
+
+
+# In[17]:
+
+
+coffee_func('Pete')
+
+
+# In[18]:
+
+
+from my_module import coffee_func, message
+
+
+# In[19]:
+
+
+message
+
+
+# In[20]:
+
+
+print(sys.path)
+
+
+# In[21]:
+
+
+import math
+
+
+# In[22]:
+
+
+print(dir(my_module))
+
+
+# In[24]:
+
+
+print(dir(math))
+
+
+# In[25]:
+
+
+print(pi)
+
+
+# In[26]:
+
+
+print(math.pi)
+
+
+# In[27]:
+
+
+print(math.cos(math.radians(90)))
+
+
+# In[28]:
+
+
+import random
+
+
+# In[29]:
+
+
+courses = ['History', 'Math', 'Physics', 'Art', 'Music']
+
+
+# In[40]:
+
+
+random_course = random.choice(courses)
+print(random_course)
+
+
+# In[41]:
+
+
+print(random_course)
+
+
+# In[42]:
+
+
+print(random_course)
+
+
+# In[43]:
+
+
+import datetime, calendar
+
+
+# In[44]:
+
+
+datetime.date.today()
+
+
+# In[45]:
+
+
+print(datetime.date.today())
+
+
+# In[46]:
+
+
+print(calendar.isleap(2022))
+
+
+# In[47]:
+
+
+print(calendar.isleap(2024))
+
+
+# In[ ]:
+
+
+
+
---
+++ my_module.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def coffee_func(person='Bugs'):
+    return 'coffee time for {}'.format(person)
+
+
+# In[2]:
+
+
+message = 'It\'s lunch time'
+
